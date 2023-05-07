077e0bdb4f6ed48a3add36e644715e04dbe9bbd5
KonstantinLymarenko <Kvlymarenko@gmail.com>
2023-05-07 15:39:51 +0300

    upd-sl-js

--- src/js/slider.js
+++ src/js/slider.js
@@ -17,7 +17,17 @@ const swiper = new Swiper('.mySwiper', {
     nextEl: '.swiper-btn-next',
     prevEl: '.swiper-btn-prev',
   },
-
+  on: {
+    slideChange: function () {
+      var currentSlide = this.realIndex + 1;
+      var totalSlides = this.slides.length;
+      document.querySelector('.first-slide-num').innerHTML =
+        currentSlide < 10 ? '0' + currentSlide : currentSlide;
+      document.querySelector('.last-slide-num').innerHTML =
+        totalSlides < 10 ? '0' + totalSlides : totalSlides;
+    },
+  },
+ 
   // And if we need scrollbar
   // scrollbar: {
   //   el: '.swiper-scrollbar',
